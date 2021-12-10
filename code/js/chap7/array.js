67d686c67a353b3305cc7367ab2bae8190c99480
masaikk <201830660420@mail.scut.edu.cn>
2021-12-10 22:07:15 +0800

    12/10-2

--- code/js/chap7/array.js
+++ code/js/chap7/array.js
@@ -2,8 +2,14 @@
 //
 // console.log(data.filter((x, i) => i % 2 === 0));//[ 1, 3, 5, undefined ]
 
-let a = [1, [2, [3, [4]]]]
-console.log(a.flat(1));//[ 1, 2, [ 3, [ 4 ] ] ]
-console.log(a.flat(2));//[ 1, 2, 3, [ 4 ] ]
-console.log(a.flat(3));//[ 1, 2, 3, 4 ]
-console.log(a.flat(4));//[ 1, 2, 3, 4 ]
+// let a = [1, [2, [3, [4]]]]
+// console.log(a.flat(1));//[ 1, 2, [ 3, [ 4 ] ] ]
+// console.log(a.flat(2));//[ 1, 2, 3, [ 4 ] ]
+// console.log(a.flat(3));//[ 1, 2, 3, 4 ]
+// console.log(a.flat(4));//[ 1, 2, 3, 4 ]
+
+
+let a = [1, 2, 3, 4, 5];
+console.log(a.slice(0, 3));//[1, 2, 3]
+console.log(a.slice(3));//[4, 5]
+console.log(a.slice(1, -1));//[2, 3, 4]
