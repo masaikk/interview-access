0c21ed8ec1e83d72dee53bef926526045bfbf130
masaikk <201830660420@mail.scut.edu.cn>
2022-01-02 16:24:45 +0800

    1/2-2

--- code/cpp/other/fork.cpp
+++ code/cpp/other/fork.cpp
@@ -2,7 +2,11 @@
 // Created by b8313 on 2022/1/2.
 //
 
-#include "fork.h"
+//#include "fork.h"
+
+#include <unistd.h>
+#include <stdio.h>
+
 
 #include <unistd.h>
 #include <stdio.h>
@@ -12,19 +16,27 @@ int main(){
     // 创建进程
     pid_t pid = fork();
 
+    // 局部变量
+    int num = 10;
+
     // 判断当前进程是父进程 还是子进程
     if (pid > 0){			// 进程号 > 0，即为子进程的进程号，当前为父进程
-        printf("pid: %d\n", pid);
         printf("I am parent process, pid: %d, ppid: %d\n", getpid(), getppid());
+
+        printf("parent process num : %d\n", num);
+        num += 10;
+        printf("parent process num + 10 : %d\n", num);
+
     }
     else if (pid == 0){		// 进程号 == 0，表示当前为子进程
         printf("I am child process, pid: %d, ppid: %d\n", getpid(), getppid());
+
+        printf("child process num : %d\n", num);
+        num += 100;
+        printf("child process num + 100 : %d\n", num);
     }
 
-    for (int i = 0; i < 5; i++){
-        printf("pid: %d, i : %d\n", getpid(), i);
-        sleep(1);
-    }
     return 0;
 }
+
 
--- know.md
+++ know.md
@@ -139,3 +139,34 @@ int main(){
 }
 ```
 
+进程之间的变量不会互相影响
+
+![image-20220102162315104](know.assets/image-20220102162315104.png)
+
+代码如下:
+
+```c++
+    // 创建进程
+    pid_t pid = fork();
+
+    // 局部变量
+    int num = 10;
+
+    // 判断当前进程是父进程 还是子进程
+    if (pid > 0){			// 进程号 > 0，即为子进程的进程号，当前为父进程
+        printf("I am parent process, pid: %d, ppid: %d\n", getpid(), getppid());
+
+        printf("parent process num : %d\n", num);
+        num += 10;
+        printf("parent process num + 10 : %d\n", num);
+
+    }
+    else if (pid == 0){		// 进程号 == 0，表示当前为子进程
+        printf("I am child process, pid: %d, ppid: %d\n", getpid(), getppid());
+
+        printf("child process num : %d\n", num);
+        num += 100;
+        printf("child process num + 100 : %d\n", num);
+    }
+```
+
