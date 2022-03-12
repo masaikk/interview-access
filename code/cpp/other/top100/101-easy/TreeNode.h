2c69051c686c398b3878fd9a4728dd6f69182801
masaikk <201830660420@mail.scut.edu.cn>
2022-03-12 13:38:06 +0800

    3/12-1

---
+++ code/cpp/other/top100/101-easy/TreeNode.h
@@ -0,0 +1,23 @@
+//
+// Created by b8313 on 2022/3/12.
+//
+
+#ifndef INTERVIEW_ACCESS_TREENODE_H
+#define INTERVIEW_ACCESS_TREENODE_H
+
+
+//* Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
+#endif //INTERVIEW_ACCESS_TREENODE_H
