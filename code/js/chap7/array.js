c030d1f4d038d785f0a40c428f449cb6be78dd95
masaikk <201830660420@mail.scut.edu.cn>
2022-03-18 17:33:52 +0800

    3/18-2

--- code/js/chap7/array.js
+++ code/js/chap7/array.js
@@ -9,7 +9,28 @@
 // console.log(a.flat(4));//[ 1, 2, 3, 4 ]
 
 
-let a = [1, 2, 3, 4, 5];
-console.log(a.slice(0, 3));//[1, 2, 3]
-console.log(a.slice(3));//[4, 5]
-console.log(a.slice(1, -1));//[2, 3, 4]
+// let a = [1, 2, 3, 4, 5];
+// console.log(a.slice(0, 3));//[1, 2, 3]
+// console.log(a.slice(3));//[4, 5]
+// console.log(a.slice(1, -1));//[2, 3, 4]
+
+let a=[
+    {'d1':100,'d2':3,'d3':'aerwa'},
+    {'d1':5,'d2':5,'d3':'zzaa'},
+    {'d1':12,'d2':3,'d3':'gg'},
+    {'d1':-9,'d2':87,'d3':'sfaa'},
+    {'d1':6,'d2':3,'d3':'aaa'},
+]
+
+let b=a.sort((a,b)=>{
+    return a.d1>b.d1?1:-1;
+})
+
+console.log(b);
+//[
+//   { d1: 100, d2: 3, d3: 'aerwa' },
+//   { d1: 5, d2: 5, d3: 'zzaa' },
+//   { d1: 12, d2: 3, d3: 'gg' },
+//   { d1: -9, d2: 87, d3: 'sfaa' },
+//   { d1: 6, d2: 3, d3: 'aaa' }
+// ]
